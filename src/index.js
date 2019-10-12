33a35f9d0661bc95408ccb99ebb7958fbbe96a7c
Bo <peterbozhou@gmail.com>
2019-10-12 17:45:54 -0400

    Added Timeline

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Timeline from './components/timeline';
+import Todo from './components/todo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Timeline />, document.getElementById('timeline'));
+ReactDOM.render(<Todo/>, document.getElementById('todo'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

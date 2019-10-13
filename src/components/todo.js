b88624a12e27765bba86dbd1457c21fd4b117077
Meng <mengl@live.unc.edu>
2019-10-12 22:24:07 -0400

    new-style

--- src/components/todo.js
+++ src/components/todo.js
@@ -6,13 +6,17 @@ class Todo extends Component {
   };
 
   addItem = () => {
-    this.setState({
-      todoList: [
-        ...this.state.todoList,
-        { id: Date.now(), content: document.getElementById("newList").value }
-      ]
-    });
-    document.getElementById("newList").value = "";
+    if (document.getElementById("newList").value == "") {
+      alert("must have a value");
+    } else {
+      this.setState({
+        todoList: [
+          ...this.state.todoList,
+          { id: Date.now(), content: document.getElementById("newList").value }
+        ]
+      });
+      document.getElementById("newList").value = "";
+    }
   };
 
   removeItem = e => {
@@ -34,23 +38,52 @@ class Todo extends Component {
   render() {
     return (
       <React.Fragment>
-        <ul>
+        <form className="my-3">
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="text"
+                className="form-control"
+                id="newList"
+                placeholder="new task"
+              ></input>
+            </div>
+            <div className="col-4 d-flex justify-content-end">
+              <button
+                type="button"
+                onClick={this.addItem}
+                className="btn btn-info mb-2 btn-block"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
+        <ul className="list-group">
           {this.state.todoList.map(li => (
-            <li key={li.id} id={li.id}>
-              {li.content}
-              <input type="checkbox" onChange={e => this.checked(e)}></input>
-              <button type="button" onClick={e => this.removeItem(e)}>
-                -
+            <li
+              className="list-group-item d-flex justify-content-between align-items-center"
+              key={li.id}
+              id={li.id}
+            >
+              <span>
+                <input
+                  type="checkbox"
+                  onChange={e => this.checked(e)}
+                  className="mr-2"
+                ></input>
+                {li.content}
+              </span>
+              <button
+                className="btn btn-outline-info btn-sm"
+                type="button"
+                onClick={e => this.removeItem(e)}
+              >
+                X
               </button>
             </li>
           ))}
         </ul>
-        <form>
-          <input type="text" id="newList"></input>
-          <button type="button" onClick={this.addItem}>
-            Add to List
-          </button>
-        </form>
       </React.Fragment>
     );
   }

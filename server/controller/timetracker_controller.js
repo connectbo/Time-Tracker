339b417948d12378b83ff0c5ab9c947ccf463d4a
Bo <peterbozhou@gmail.com>
2019-10-13 01:05:45 -0400

    Pushed

--- server/controller/timetracker_controller.js
+++ server/controller/timetracker_controller.js
@@ -1,7 +1,6 @@
 const sql = require('../config/db');
 
 exports.signup = async function (req, res){
-    console.log("Signup visited")
     const toquery = "INSERT into user (user_name,user_password) values(?, ?)";
     sql.query(toquery, [req.user, req.password], function (err, res){
         if(err){
@@ -30,6 +29,19 @@ exports.login = async function(req, res){
 exports.insertActivity = async function (req, res){
     const toquery = "INSERT into activity (activity_title,user_id) values(?, ?)";
     sql.query(toquery, [req.activity_title, req.user_id], function (err, res){
+        if(err){
+            console.log("Error: ", err);
+            res(err,null);
+        }
+        else{
+            console.log(res);
+        }
+    })
+}
+
+exports.deleteActivity = async function (req, res){
+    const toquery = "DELETE from activity (activity_id, activity_title,user_id) where activity_id = ? AND user_id = ?)";
+    sql.query(toquery, [req.activity_id, req.user_id], function (err, res){
         if(err){
             console.log("Error: ", err);
             res(err,null);
